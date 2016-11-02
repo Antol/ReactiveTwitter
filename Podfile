74bb3503aa35a6a36513b58d96f1498af4e798e6
Antol <antol.peshkov@gmail.com>
2016-11-02 17:18:26 +0000

    Get tweets

--- Podfile
+++ Podfile
@@ -4,6 +4,9 @@ inhibit_all_warnings!
 
 target 'ReactiveTwitter' do
   pod 'ReactiveCocoa', '~> 2.5'
-  pod 'AFNetworking', '~> 3.1'
-  pod 'Realm', '~> 2.0.2'
+  pod 'AFNetworking'
+  pod 'Realm'
+  pod 'Fabric'
+  pod 'TwitterKit'
+  pod 'TwitterCore'
 end
--- ReactiveTwitter/ViewController.m
+++ ReactiveTwitter/ViewController.m
@@ -7,23 +7,44 @@
 //
 
 #import "ViewController.h"
+#import <TwitterKit/TwitterKit.h>
+#import "ReactiveCocoa.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) TWTRAPIClient *client;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad];
+    
+    @weakify(self);
+    [[Twitter sharedInstance] logInWithCompletion:^(TWTRSession * _Nullable session, NSError * _Nullable error) {
+        @strongify(self);
+        self.client = [[TWTRAPIClient alloc] initWithUserID:session.userID];
+        NSString *statusesShowEndpoint = @"https://api.twitter.com/1.1/statuses/home_timeline.json";
+        NSError *clientError;
+        
+        NSURLRequest *request = [self.client URLRequestWithMethod:@"GET" URL:statusesShowEndpoint parameters:nil error:&clientError];
+        
+        if (request) {
+            [self.client sendTwitterRequest:request completion:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                if (data) {
+                    NSError *jsonError;
+                    id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                    NSArray *tweets = [TWTRTweet tweetsWithJSONArray:json];
+                    NSLog(@"================> %@", tweets);
+                }
+                else {
+                    NSLog(@"Error: %@", connectionError);
+                }
+            }];
+        }
+        else {
+            NSLog(@"Error: %@", clientError);
+        }
+    }];
 }
-
-
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
-}
-
 
 @end
